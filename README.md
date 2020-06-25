f872ed14376582b00c5a6760628e3f9e0e16da2f
Vinayabhilash <66827966+Vinayabhilash@users.noreply.github.com>
2020-06-25 15:05:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project-GSCO
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,39 @@
+var zenia;
+var wall1;
+var speed, weight;
+
+function setup() {
+  createCanvas(800,400);
+  zenia = createSprite(400, 200, 20, 20);
+
+  wall1 = createSprite(700, 200, 20, 50);
+  
+  speed = random(55, 90);
+  weight = random(400,1500);
+
+  
+}
+
+function draw() {
+  background(255,255,255);
+  zenia.velocityX = speed;
+
+  if(wall1.x - zenia.x < (zenia.width + wall1.width)/2) {
+    zenia.velocityX = 0;
+
+    var deformation = 0.5 * weight * speed * speed/22509;
+
+    if(deformation > 180) {
+      zenia.shapeColor=color(255,0,0);
+    }
+
+    if(deformation < 180 && deformation > 100) {
+      zenia.shapeColor=color(230, 230, 0);
+    }
+
+    if(deformation < 100) {
+      zenia.shapeColor=color(0,255,0);
+    }
+  }
+  drawSprites();
+}
